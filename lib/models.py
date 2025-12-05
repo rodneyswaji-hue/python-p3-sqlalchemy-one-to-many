08df897b32ec0ed6ff2142d9ccc9127b22bb85d8
Rodney Swaji <rodney.swaji@student.moringaschool.com>
2025-12-05 08:45:47 +0300

    initial commit

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,5 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, create_engine
+from sqlalchemy.orm import relationship, backref, sessionmaker, declarative_base
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -8,3 +7,37 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+engine = create_engine("sqlite:///games.db")
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    genre = Column(String)
+    platform = Column(String)
+    price = Column(Integer)
+
+    reviews = relationship('Review', backref=backref('game'))
+
+    def __repr__(self):
+        return f"Game(id={self.id}, title={self.title}, platform={self.platform})"
+
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer, primary_key=True)
+    score = Column(Integer)
+    comment = Column(String)
+    game_id = Column(Integer, ForeignKey("games.id"))
+
+    def __repr__(self):
+        return f"Review(id={self.id}, score={self.score}, game_id={self.game_id})"
+
+
+Base.metadata.create_all(engine)
